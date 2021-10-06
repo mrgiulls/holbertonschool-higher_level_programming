d7f45063ceb025b8b40d4cb02a3296b47ea7065c
mrgiulls <giuliano.mesias@hotmail.com>
2021-10-06 08:52:55 -0500

    test-drivn development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-"""0. Integers addition"""
-
-
 def add_integer(a, b=98):
-    """adds two integers
-
-    Args:
-        a: must be of type int or raises an exception
-        b: must be of type int or raises an exception
-
-    Returns:
-        The sum of a and b
-    """
-    if not(type(a) in (int, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    elif not(type(b) in (int, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    else:
-        return int(a) + int(b)
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
