53c12a3c97247ad1380f9953b88f8ff9a6494083
mrgiulls <giuliano.mesias@hotmail.com>
2022-05-26 23:09:13 -0500

    task8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$(function () {
+  $.getJSON("https://swapi-api.hbtn.io/api/films/?format=json", function (data) {
+    $("UL#list_movies").append(...data.results.map(movie => `<li>${movie.title}</li>`));
+  });
+})
